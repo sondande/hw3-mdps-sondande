b05ebec9bad81b7c3a7acc575373e1d3160be6d5
Sagana Ondande <sondande@oberlin.edu>
2021-12-06 00:12:56 -0500

    Update with more steps for value iteration

--- mdp_planner.py
+++ mdp_planner.py
@@ -19,6 +19,22 @@ def value_iteration(mdp):
             action_list.append(0)
         v_table.append(action_list)
 
+    # (Aka step 4. Repeat Steps 2-3 until V values converge * Largest change in V(s) is less than e)
+    # While loop (until the largest change in V < e aka the covergence criteria is met)
+    # TODO add after testing 2-3 and ensuring it is correct
+
+    # Step 2: Update entire Q table using Bellman equation
+    for s in mdp[0]:
+        for a in mdp[1]:
+            # Calculate Q(s, a) with Bellman
+            value = 0
+            # See if the reward from our state to the next state is in our MDP. If not, use the value 0 where we need it
+
+
+            # TODO if we run into an error that a possible transition doesn't have a reward or a reward doens't have a transition, the value is zero. Was specified in the homework assignment listing as
+            # The ones not listed in the MDP have values of 0 for probability or reward value. Dependant on which one is missing
+
+
     return
 
 
@@ -50,6 +66,7 @@ def main():
     # Going through states, splitting into id and label, removing \n and putting in dictionary
     while line != '\n':
         data = line.split(",")
+        print(data)
         label = data[1]
         label = label[:-1]
         states[data[0]] = label
@@ -60,6 +77,7 @@ def main():
     # going through actions, splitting into id and label, removing \n and putting in dictionary
     while line != '\n':
         data = line.split(",")
+        print(data)
         label = data[1]
         label = label[:-1]
         actions[data[0]] = label
@@ -70,6 +88,7 @@ def main():
     # going through state transitions, splitting into 4 parts and nesting them into layers in the dictionary
     while line != '\n':
         data = line.split(",")
+        print(data)
         label = data[3]
         label = label[:-1]
         last = {}
@@ -82,6 +101,7 @@ def main():
     line = next(f)
 
     # going through rewards, splitting into 4 parts and nesting them into layers in the dictionary
+    # TODO when using a debugger, I found that the size of the dictionary for rewards was only 9 instead of the expected 18. This also happens in state transitions
     for line in f:
         data = line.split(",")
         label = data[3]
